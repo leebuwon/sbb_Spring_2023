ab4788f02d4d774ec7cb2be1ae580e5cd6a064e7
bw1611 <bw1611@naver.com>
2023-03-23 13:29:34 +0900

    3-10 댓글 수정, 삭제 추가

--- src/main/java/com/mysite/sbb/domain/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/domain/answer/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.domain.answer.controller;
 
 import com.mysite.sbb.domain.answer.dto.CreateAnswerDto;
+import com.mysite.sbb.domain.answer.entity.Answer;
 import com.mysite.sbb.domain.answer.service.AnswerService;
 import com.mysite.sbb.domain.question.dto.CreateQuestionDto;
 import com.mysite.sbb.domain.question.entity.Question;
@@ -9,14 +10,13 @@ import com.mysite.sbb.domain.user.entity.SiteUser;
 import com.mysite.sbb.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -44,5 +44,47 @@ public class AnswerController {
 
         answerService.create(question, createAnswerDto.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modifyAnswer(CreateAnswerDto createAnswerDto, @PathVariable Integer id,
+                               Principal principal){
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answer.setContent(answer.getContent());
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("modify/{id}")
+    public String modifyAnswer(@Valid CreateAnswerDto createAnswerDto, BindingResult bindingResult,
+                               @PathVariable Integer id, Principal principal){
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.answerService.modify(answer, createAnswerDto.getContent());
+
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String deleteAnswer(Principal principal, @PathVariable Integer id){
+        Answer answer = this.answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+
+        this.answerService.delete(answer);
+
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 }
--- src/main/java/com/mysite/sbb/domain/answer/service/AnswerService.java
+++ src/main/java/com/mysite/sbb/domain/answer/service/AnswerService.java
@@ -3,11 +3,13 @@ package com.mysite.sbb.domain.answer.service;
 import com.mysite.sbb.domain.answer.entity.Answer;
 import com.mysite.sbb.domain.answer.repository.AnswerRepository;
 import com.mysite.sbb.domain.question.entity.Question;
+import com.mysite.sbb.domain.question.exception.DataNotFoundException;
 import com.mysite.sbb.domain.user.entity.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = answerRepository.findById(id);
+        if (answer.isPresent()){
+            return answer.get();
+        }else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer){
+        answerRepository.delete(answer);
     }
 }
